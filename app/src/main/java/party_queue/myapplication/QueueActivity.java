7dc74adf56ca637eb504aa6493a18f3d5995c934
Bu Sun Kim <busunkim96@gmail.com>
2016-04-23 14:57:18 -0600

    dropdown working

--- app/src/main/java/party_queue/myapplication/QueueActivity.java
+++ app/src/main/java/party_queue/myapplication/QueueActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,10 +13,8 @@ import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.*;
+import com.android.volley.*;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,8 +31,12 @@ public class QueueActivity extends AppCompatActivity {
 
     List<Data> data;
     RecyclerView recyclerView;
+
     @Bind(R.id.searchText)
     EditText _searchText;
+
+    RequestQueue queue;
+
     Integer numResultsToShow = new Integer(10);
     Recycler_View_Adapter adapter;
 
@@ -54,7 +57,7 @@ public class QueueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue);
-        ButterKnife.bind(this);
+
         fill_with_data();
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
@@ -62,92 +65,94 @@ public class QueueActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        ButterKnife.bind(this);
 
-    };
+        queue = Volley.newRequestQueue(this);
+    }
 
 
 
+    public void searchButtonClick(View v)
+    {
+        // do something when search button is clicked
+        String search = _searchText.getText().toString(); //grabs string from _searchText text box defined in activity_spotify.xml
+        //JsonObjectRequest jsObj = new JsonObjectRequest
+        //jsObj= Request.Method.GET, "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist"
+        final Button button = (Button) v;
+        ( (Button) v).setText(search); // sets the text inside the button to be the text from the text box
 
-    public void searchButtonClick(View v) {
-        //if (v.getId() == R.id.button) {
-            // do something when search button is clicked
-            String search = _searchText.getText().toString(); //grabs string from _searchText text box defined in activity_spotify.xml
-            //JsonObjectRequest jsObj = new JsonObjectRequest
-            //jsObj= Request.Method.GET, "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist"
-            final Button button = (Button) v;
-            ((Button) v).setText(search); // sets the text inside the button to be the text from the text box
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, "https://api.spotify.com/v1/search?q="+search+"*&type=track&limit=10" , null, new Response.Listener<JSONObject>() {
 
-            JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                    (Request.Method.GET, "https://api.spotify.com/v1/search?q=" + search + "*&type=track&limit=10", null, new Response.Listener<JSONObject>() {
-
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            // mTxtDisplay.setText("Response: " + response.toString());
-                            String[] songId = new String[numResultsToShow];
-                            String[] artistnames = new String[numResultsToShow];
-                            String[] songName = new String[numResultsToShow];
-                            try {
-                                for (int i = 0; i < 10; i++) {
-                                    //artist = response.getJSONArray("artists").getJSONObject(0).getString("name");
-                                    songId[i] = response.getJSONObject("tracks").getJSONArray("items").getJSONObject(i).getString("id");
-                                    artistnames[i] = response.getJSONObject("tracks").getJSONArray("items").getJSONObject(i).getJSONArray("artists").getJSONObject(0).getString("name");
-                                    songName[i] = response.getJSONObject("tracks").getJSONArray("items").getJSONObject(i).getString("name");
-                                }
-
-                            } catch (JSONException e) {
-                            }
-                            //button1 = (Button) findViewById(R.id.button1);
-
-                            //Creating the instance of PopupMenu
-                            PopupMenu popup = new PopupMenu(QueueActivity.this, button);
-                            //Inflating the Popup using xml file
-                            popup.getMenuInflater()
-                                    .inflate(R.menu.popup_menu, popup.getMenu());
-
-                            for (int i = 0; i < numResultsToShow; i++) {
-                                popup.getMenu().add("Artist: " + artistnames[i] + "\n" + "Song: " + songName[i]);
-                                //popup.getMenu().getItem(i).setTitle(artistnames[i]);
-                            }
-                            //registering popup with OnMenuItemClickListener
-                            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                                public boolean onMenuItemClick(MenuItem item) {
-                                    //data.add(new Data(item.getTitle().toString(), "http://www.zyekil.com/wp-content/uploads/2015/12/wolf_cover_art-1.jpg", "new song uri is invisible"));
-
-                                    //adapter.notifyDataSetChanged();
-
-                                    Toast.makeText(
-                                            QueueActivity.this,
-                                            item.getTitle() + " added to queue",
-                                            Toast.LENGTH_SHORT
-                                    ).show();
-                                    return true;
-                                }
-                            });
-
-                            popup.show(); //showing popup menu
-                            //if (!img.isEmpty()) mpv.setCoverURL(img);
-                            //else mpv.setCoverURL("https://upload.wikimedia.org/wikipedia/en/b/b3/MichaelsNumberOnes.JPG");
-                            //if (!artist.isEmpty()) artistView.setText(artist);
-                            //if (!song.isEmpty()) songView.setText(song);
-                            for (int i = 0; i < 10; i++) {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // mTxtDisplay.setText("Response: " + response.toString());
+                        String[] songId = new String[numResultsToShow];
+                        String[] artistnames = new String[numResultsToShow];
+                        String[] songName = new String[numResultsToShow];
+                        try {
+                            for (int i=0; i<10; i++) {
                                 //artist = response.getJSONArray("artists").getJSONObject(0).getString("name");
-                                Log.d("SpotifyActivity", "songs[" + i + "] = " + songId[i]);
-                                Log.d("SpotifyActivity", "artist[" + i + "] = " + artistnames[i]);
+                                songId[i] = response.getJSONObject("tracks").getJSONArray("items").getJSONObject(i).getString("id");
+                                artistnames[i] = response.getJSONObject("tracks").getJSONArray("items").getJSONObject(i).getJSONArray("artists").getJSONObject(0).getString("name");
+                                songName[i] = response.getJSONObject("tracks").getJSONArray("items").getJSONObject(i).getString("name");
                             }
+
+                        } catch (JSONException e) {}
+                        //button1 = (Button) findViewById(R.id.button1);
+
+                        //Creating the instance of PopupMenu
+                        PopupMenu popup = new PopupMenu(QueueActivity.this, button);
+                        //Inflating the Popup using xml file
+                        popup.getMenuInflater()
+                                .inflate(R.menu.popup_menu, popup.getMenu());
+
+                        for (int i = 0; i<numResultsToShow; i++) {
+                            popup.getMenu().add("Artist: "+ artistnames[i]+ "\n" + "Song: " + songName[i]);
+                            //popup.getMenu().getItem(i).setTitle(artistnames[i]);
                         }
-                    }, new Response.ErrorListener() {
+                        //registering popup with OnMenuItemClickListener
+                        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                            public boolean onMenuItemClick(MenuItem item) {
+                                Toast.makeText(
+                                        QueueActivity.this,
+                                        item.getTitle() + " added to queue",
+                                        Toast.LENGTH_SHORT
+                                ).show();
 
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            // TODO Auto-generated method stub
-                            Log.d("SpotifyActivity", "ERRorlistenerdeal");
+                                data.add(new Data(item.getTitle().toString(), "http://www.zyekil.com/wp-content/uploads/2015/12/wolf_cover_art-1.jpg", "new song uri is invisible"));
+                                adapter.notifyDataSetChanged();
+                                return true;
+                            }
+                        });
+
+                        popup.show(); //showing popup menu
+                        //if (!img.isEmpty()) mpv.setCoverURL(img);
+                        //else mpv.setCoverURL("https://upload.wikimedia.org/wikipedia/en/b/b3/MichaelsNumberOnes.JPG");
+                        //if (!artist.isEmpty()) artistView.setText(artist);
+                        //if (!song.isEmpty()) songView.setText(song);
+                        for (int i=0; i<10; i++) {
+                            //artist = response.getJSONArray("artists").getJSONObject(0).getString("name");
+                            Log.d("SpotifyActivity", "songs[" + i +"] = " + songId[i]);
+                            Log.d("SpotifyActivity", "artist[" + i +"] = " + artistnames[i]);
 
                         }
-                    });
-            //queue.add(jsObjRequest);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        Log.d("SpotifyActivity", "ERRorlistenerdeal");
+
+                    }
+                });
+        queue.add(jsObjRequest);
 
 
-        }
-    //}
+
+    }
+
+
 
 }
